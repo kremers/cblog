97c6a7e177be0322ca8f25bab434cfde4bfbef2e
Martin Kremers <kremers@act-online.de>
2012-04-04 13:09:20 +0200

    No slash appending removal any more, see: http://googlewebmastercentral.blogspot.de/2010/04/to-slash-or-not-to-slash.html

--- src/cblog/util.clj
+++ src/cblog/util.clj
@@ -19,28 +19,3 @@
 ; Generate timestamp
 (defn gen-timestamp [] (unparse (formatters :date-hour-minute-second) (now)))
 
-
-; The next "big" section is middleware to ignore the trailing /
-(defn with-uri-rewrite
-    "Rewrites a request uri with the result of calling f with the
-       request's original uri.  If f returns nil the handler is not called."
-    [handler f]
-    (fn [request]
-          (let [uri (:uri request)
-                          rewrite (f uri)]
-                  (if rewrite
-                            (handler (assoc request :uri rewrite))
-                            nil))))
-
-(defn- uri-snip-slash
-    "Removes a trailing slash from all uris except \"/\"."
-    [uri]
-    (if (and (not (= "/" uri))
-                        (.endsWith uri "/"))
-          (trim-newline uri)
-          uri))
-
-(defn ignore-trailing-slash
-    "Makes routes match regardless of whether or not a uri ends in a slash."
-    [handler]
-    (with-uri-rewrite handler uri-snip-slash))
