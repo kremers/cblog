4b6920e80274cc7aa42be0ea214b074639c568db
Martin Kremers <kremers@act-online.de>
2012-04-10 22:31:06 +0200

    Try 4

--- src/cblog/core.clj
+++ src/cblog/core.clj
@@ -1,12 +1,15 @@
 (ns cblog.core
+  (:refer-clojure :exclude [count])
   (:use [cblog routes db]
         [clojure.tools.logging :only (info error)]
         [ring.adapter.jetty] [monger.core :as m]))
 
 ;(info "entering main")
 ; connect to db!
-(let [x (zipmap [:user :password :host :db] (rest (first (re-seq  #"[\\/]{2}([^:]+):([^@]+)@([^/]+)/(.+)" (System/getenv "MONGOHQ_URL")))))]
- (do (info (System/getenv "MONGOHQ_URL")) (m/connect! {:host (:host x)}) (m/set-db! (m/get-db (:db x))) (m/authenticate m/*mongodb-database* (:host x) (:user x) (:password x)))) 
+;(let [x (zipmap [:user :password :host :db] (rest (first (re-seq  #"[\\/]{2}([^:]+):([^@]+)@([^/]+)/(.+)" (System/getenv "MONGOHQ_URL")))))]
+; (do (info (System/getenv "MONGOHQ_URL")) (info (str "db: " (:db x))) (m/connect! {:host (:host x)}) (m/set-db! (m/get-db (:db x))) (m/authenticate (:host x) (:user x) (:password x)))) 
+
+(m/connect-via-uri! (System/getenv "MONGOHQ_URL"))
 
 ;(connect-to-db! {:host  :port})
 (def routes (init-routes!))
