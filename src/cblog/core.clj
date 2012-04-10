22c24e6c275b3e61649b65fed8053b26b6fc4515
Martin Kremers <kremers@act-online.de>
2012-04-10 22:46:06 +0200

    Try it...

--- src/cblog/core.clj
+++ src/cblog/core.clj
@@ -8,7 +8,7 @@
 ; connect to db!
 ;(let [x (zipmap [:user :password :host :db] (rest (first (re-seq  #"[\\/]{2}([^:]+):([^@]+)@([^/]+)/(.+)" (System/getenv "MONGOHQ_URL")))))]
 ; (do (info (System/getenv "MONGOHQ_URL")) (info (str "db: " (:db x))) (m/connect! {:host (:host x)}) (m/set-db! (m/get-db (:db x))) (m/authenticate (:host x) (:user x) (:password x)))) 
-
+(info  (System/getenv "MONGOHQ_URL"))
 (m/connect-via-uri! (System/getenv "MONGOHQ_URL"))
 
 ;(connect-to-db! {:host  :port})
