4954fbf3c9a3af20827a03cb82eeb498e81403f7
Martin Kremers <kremers@act-online.de>
2012-04-10 21:49:59 +0200

    Second try

--- src/cblog/core.clj
+++ src/cblog/core.clj
@@ -6,7 +6,7 @@
 ;(info "entering main")
 ; connect to db!
 (let [x (zipmap [:user :password :host] (rest (first (re-seq  #"[\\/]{2}([^:]+):([^@]+)@([^/]+)" (System/getenv "MONGOHQ_URL")))))]
- (do (m/connect! {:host (:host x)}) (m/set-db! (m/get-db "cblog")) (m/authenticate m/*mongodb-database* (:host x) (:user x) (:password x)))) 
+ (do (info (System/getenv "MONGOHQ_URL")) (m/connect! {:host (:host x)}) (m/set-db! (m/get-db "cblog")) (m/authenticate m/*mongodb-database* (:host x) (:user x) (:password x)))) 
 
 ;(connect-to-db! {:host  :port})
 (def routes (init-routes!))
