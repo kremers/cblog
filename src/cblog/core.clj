e102cbd8db0594b4af1b77a9a131e8b51822beda
Martin Kremers <kremers@act-online.de>
2012-04-14 15:06:04 +0200

    Fixed bug

--- src/cblog/core.clj
+++ src/cblog/core.clj
@@ -6,8 +6,8 @@
 
 (def routes (init-routes!))
 
-(try (let [mongo-url (System/getenv "MONGOHQ_URL")] (do (info (str (-> mongo-url "MONGOHQ_URL"))) (m/connect-via-uri! mongo-url))) 
-  (catch Exception e (do (info "MONGOHQ_URL not set, using localhost") (connect-to-db!))))
+(try (let [mongo-url (System/getenv "MONGOHQ_URL")] (do (info mongo-url) (m/connect-via-uri! mongo-url))) 
+  (catch NullPointerException e (do (info "MONGOHQ_URL not set, using localhost") (connect-to-db!))))
 
 (defn -main [] (try (let [port (Integer/parseInt (System/getenv "PORT"))] (do (info (str (-> port "PORT: "))) (run-jetty routes {:port port})))
   (catch Exception e (do (info "PORT not set, using 8080") (run-jetty routes {:port 8080})))))
