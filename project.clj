1d437a7250e66a95b9c5995ac8f0c307a7253fcf
Martin Kremers <kremers@act-online.de>
2012-05-06 20:49:37 +0200

    Different image handling functionality, still problems with Piped input and output

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
                  [net.cgrand/moustache "1.1.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.slf4j/slf4j-api "1.6.4"]
+                 [org.slf4j/jcl-over-slf4j "1.6.4"]
                  [cheshire "2.0.4"]
                  [ch.qos.logback/logback-classic "1.0.0"]
                  [stencil "0.2.0"]
--- src/cblog/media.clj
+++ src/cblog/media.clj
@@ -1,5 +1,6 @@
 (ns cblog.media
-  (:import [net.coobird.thumbnailator.Thumbnails] [java.io PrintWriter PipedInputStream PipedOutputStream] )
+  (:import [net.coobird.thumbnailator.Thumbnails] [java.io PrintWriter PipedInputStream PipedOutputStream]
+           [org.apache.commons.io.output CountingOutputStream] )
   (:require [aws.sdk.s3 :as s3] [ring.util.mime-type :as mt] )
   (:use  [cheshire.core] [cblog.db] [cblog.util] [clojure.tools.logging :only [info debug error]]
         [clojure.java.io :only (as-url)]))
@@ -8,20 +9,34 @@
 (defn bucket [] (let [settings (settings-overview)] (:s3bucketname settings)))
 (defn media_list [] (let [objects (s3/list-objects (cred) (bucket))] (do (debug (with-out-str (clojure.pprint/pprint objects))) objects )))
 (defn media_upload [k v & [metad]] (do (s3/put-object (cred) (bucket) k v metad) (s3/update-object-acl (cred) (bucket) k (s3/grant :all-users :read))))
-(defn media_remove [k] (s3/delete-bucket (cred) k))
+(defn media_remove [k] (s3/delete-object (cred) (bucket) k))
 (defn media_get [k] (slurp (:content (s3/get-object (cred) (bucket) k))))
 (defn media-exists? [k] (s3/object-exists? (cred) (bucket) k))
 (defn medialist_json [] (generate-string (:objects (media_list))))
-(defn handle-submit [req] (let [filename ((:query-params req) "qqfile") input (:body req) clength (:content-length req)] 
+
+(defn gen-thumbnail "returns input stream to image" [url x y] 
+  (let [in-stream (PipedInputStream.)]  
+  (future (with-open [out (-> in-stream (PipedOutputStream.))]
+    (-> (net.coobird.thumbnailator.Thumbnails/fromURLs [(as-url url)]) (.size x y) (.outputFormat "jpg") (.toOutputStream out)))) in-stream))
+
+(defn tgen [src x y] (let [fspl (re-find #"([^/]+)\.([^\.]+)$" src) fname (fspl 1) fext (fspl 2) thumbnail (gen-thumbnail src x y)] 
+  (do (info (str "starting tgen of " src))
+      (media_upload (str "thumbnails/" fname "_" x "_" y "." fext) thumbnail {:content-type "image/jpeg"})
+      (info (str "finished tgen of " src)))))
+
+(defn handle-submit [req] (let [filename (clojure.string/lower-case ((:query-params req) "qqfile")) 
+                                input (:body req) 
+                                clength (:content-length req) 
+                                ctype (mt/ext-mime-type filename) ] 
                            (do (debug (with-out-str (clojure.pprint/pprint req))) 
-                            (media_upload filename input {:content-length clength :content-type (mt/ext-mime-type filename)}))))
+                            (info (str "uploading " filename))
+                            (media_upload filename input {:content-length clength :content-type ctype})
+                            (info (str "upload ok " filename))
+                            (when (re-matches #"^image.+" ctype) (tgen (str "http://s3.amazonaws.com/" (bucket) "/" filename) 150 150)))))
+
 (defn media_redirect [key] {:status 301 :headers {"Location" (str "http://s3.amazonaws.com/" (bucket) "/" key)} :body ""})
 (defn delete-media [req] (media_remove (:key (json-in req))))
 
-(defn gen-thumbnail "returns input stream to image" [url] 
-  (let [in-stream (PipedInputStream.)]  
-  (future (with-open [out (-> in-stream (PipedOutputStream.))]
-      (-> (net.coobird.thumbnailator.Thumbnails/fromURLs [(as-url url)]) (.size 150 150) (.outputFormat "jpg") (.toOutputStream out)))) in-stream))
 
 
 
