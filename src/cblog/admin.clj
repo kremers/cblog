6655bad47b8825ebab71ebf2cd10e914200895b9
Martin Kremers <kremers@act-online.de>
2012-05-13 22:40:48 +0200

    DRYified

--- src/cblog/admin.clj
+++ src/cblog/admin.clj
@@ -2,21 +2,21 @@
   (:import [org.bson.types ObjectId]) 
   (:use [cblog db util] [cheshire.core] [sandbar.auth] 
         [monger.collection :only [find-maps update remove-by-id update-by-id find-map-by-id find-one-as-map insert]]
-                      [monger.operators] [clojure.tools.logging :only (info error)]))
+        [monger.operators] [clojure.tools.logging :only (info error)]))
 
 (defn update-settings [req] (let [p (json-in req) k (:key p) v (:value p) old (settings-overview)] 
     (if (contains? (+settings) (keyword k)) (:err (update "settings" {:version 0} (merge old {(keyword k) v}))) "key not available")) )
 
 (defn update-adminpw [req] (when-let [isAdmin (= (current-username) "admin")] 
                              (:err (let [p (json-in req) k (:newpw p) oldAdmin (find-one-as-map "users" {:login "admin"})] 
-                               (do (info (str "Admin password changed to: " k)) 
-                                 (update "users" {:login "admin"} (merge oldAdmin {:pass (hash-password k "hawaiian black salt")})))))))
+                                     (do (info (str "Admin password changed to: " k)) 
+                                         (update "users" {:login "admin"} (merge oldAdmin {:pass (hash-password k "hawaiian black salt")})))))))
 
 (defn admin-health [] (generate-string (let [^Runtime r (Runtime/getRuntime)] 
-                        { :freememory (int (/ (. r (freeMemory)) 1e6))
-                          :totalmemory (int (/ (. r (totalMemory)) 1e6)) 
-                          :maxmemory (int (/ (. r (maxMemory)) 1e6))
-                          :processors (int (. r (availableProcessors))) })))
+                                         { :freememory (int (/ (. r (freeMemory)) 1e6))
+                                          :totalmemory (int (/ (. r (totalMemory)) 1e6)) 
+                                          :maxmemory (int (/ (. r (maxMemory)) 1e6))
+                                          :processors (int (. r (availableProcessors))) })))
 
 (defn posts-overview [] 
   (let [posts (vec (find-maps "posts")) 
@@ -28,36 +28,39 @@
 ; Needs refactoring, :... section far too long
 (defn save-post [params]
   (let [ draft? (= (params "save") "Save Draft")
-         post   (+post { :title (params "title")
-                         :author (current-username)
-                         :category (params "category")
-                         :content (params "content")
-                         :tags    (params "tags[]")
-                         :showtitle (= "on" (params "showtitle"))
-                         :active (not draft?)})]  
-  (if-let [id (params "postid")] (update-by-id "posts" (ObjectId. id) post) (insert "posts" post))))
-
-(defn remove-post [req] (generate-string {:result (str (remove-by-id "posts" (ObjectId. (:id (json-in req)))))}))
+        post   (+post { :title (params "title")
+                       :author (current-username)
+                       :category (params "category")
+                       :content (params "content")
+                       :tags    (params "tags[]")
+                       :showtitle (= "on" (params "showtitle"))
+                       :active (not draft?)})]  
+    (if-let [id (params "postid")] (update-by-id "posts" (ObjectId. id) post) (insert "posts" post))))
 
 (defn prepare-edit [params]
   (let [categories (vec (find-maps "categories"))]
-  (if-let [id (params "id")] 
-    (let [post (find-map-by-id "posts" (ObjectId. id))] 
-      { :post post :categories (map #(if (= (:category post) (:name %1)) (assoc %1 :selected true) %1) categories) })
-  {:categories categories} ))
-)
+    (if-let [id (params "id")] 
+      (let [post (find-map-by-id "posts" (ObjectId. id))] 
+        { :post post :categories (map #(if (= (:category post) (:name %1)) (assoc %1 :selected true) %1) categories) })
+      {:categories categories} )))
 
-(defn categories-overview [] 
-  {:categories (vec (find-maps "categories"))})
-(defn remove-category [req] 
-  (generate-string {:result (str (remove-by-id "categories" (ObjectId. (:id (json-in req)))))}))
-(defn save-category [req] 
-  (generate-string {:result (str (let [data (json-in req)] (insert "categories" (+category {:name (:name data) :urlfriendly (:urlfriendly data)}))))}))
+
+(defn overview "Reads all objects from the collection as map"
+  [#^String collection] {:categories (vec (find-maps collection))} )
+(defn delete-by-id "Deletes a entity by a :id field in a json structure from a specific collection"
+  [req #^String collection] (generate-string {:result (str (remove-by-id collection (ObjectId. (:id (json-in req)))))})) 
+(defn save-new "Creates and inserts a new entity in a specified collection. Parameters get checked against map keys"
+  [req #^String collection keymap] (generate-string {:result (str (let [data (json-in req)] (insert collection (merge keymap (select-keys data (keys keymap))))))}))
+
+(defn categories-overview [] (overview "categories"))
+(defn remove-category [req] (delete-by-id req "categories"))
+(defn save-category [req] (save-new req "categories" (+category)))
+(defn remove-post [req] (delete-by-id req "posts"))
+(defn remove-link [req] (delete-by-id req "links"))
 
 (defn update-category [req] 
   (generate-string {:result (str (let [data (json-in req) 
                                        id (:id data) 
                                        category (+category {:name (:name data) :urlfriendly (:urlfriendly data)})] 
                                    (update-by-id "categories" (ObjectId. id) category)))}))
-
 
--- src/cblog/db.clj
+++ src/cblog/db.clj
@@ -5,6 +5,7 @@
         [monger.collection :only [find-maps insert find-one-as-map map-reduce]] [monger.operators]
         [monger.conversion]
         [stencil.core]
+        [clojure.string :only [lower-case]]
         [clj-time.format :only [parse unparse formatters]]
         [validateur.validation] [clojure.tools.logging :only (info error)]))
 
@@ -27,17 +28,19 @@
           :s3accesskey nil :s3secretkey nil 
           :s3bucketname nil :language "en-gb"} init))
 
+(defn +link [& [init]] (merge {:name nil :adress nil :desc nil :target "_blank" :rating 0 :category nil}))
+
 (defn map-reduce-count-tags [] (let [m "function() { if(this.tags != null) for(index in this.tags) { emit(this.tags[index], {count : 1}); } };"
                           r "function( key , values ){ var total = 0; for ( var i=0; i<values.length; i++ ) total += values[i].count;  return { count : total }; };"
                           q {}
                           res (map-reduce "posts" m r nil (com.mongodb.MapReduceCommand$OutputType/valueOf (name "INLINE")) q)]
                                 (if (>= (-> (.getRaw res) (.get "counts") (.get "input")) 2)
-                                  (sort-by #(clojure.string/lower-case (:name %))
+                                  (sort-by #(lower-case (:name %))
                                            (map #(hash-map :name (:_id %) :count (:count (:value %)))
                                                 (from-db-object (.results res) true))) nil))) 
 
 (defn count-tags [] (let [taglist (q/with-collection "posts" (q/find {:active true :category {$ne "Welcome"}}) (q/fields [:tags]))]
-          (sort-by #(clojure.string/lower-case (:name %) ) (map (fn [[x y]] {:name x :count y} ) (frequencies (filter #(not= % nil) (flatten (map #(:tags %) taglist))))))))
+          (sort-by #(lower-case (:name %) ) (map (fn [[x y]] {:name x :count y} ) (frequencies (filter #(not= % nil) (flatten (map #(:tags %) taglist))))))))
 
 (defn valid-user? [user] (let [v (validation-set (presence-of :login) (presence-of :pass)) ] (valid? v user)))
 
@@ -54,8 +57,7 @@
 (defn posts-by-urlfriendly-category [urlfriendly] 
   (let [category (:name (find-one-as-map "categories" {:urlfriendly urlfriendly}))] (posts-by-category category)))
 
-(defn all-categories []  
-  (find-maps "categories"))
+(defn all-categories [] (find-maps "categories"))
 
 (defn read-posts    ([] (read-posts 0))
                     ([limit] (for [x (q/with-collection "posts" (q/find {:active true :category {$ne "Welcome"} }) 
