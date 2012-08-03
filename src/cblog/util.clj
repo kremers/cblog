873165bfce5a96482cc6079790583563f5122172
Martin Kremers <kremers@act-online.de>
2012-08-03 14:31:31 +0200

    fix^2

--- src/cblog/util.clj
+++ src/cblog/util.clj
@@ -30,3 +30,5 @@
 (defn urlfriend [unfriendly] (s/trim (s/replace (s/replace (s/lower-case unfriendly) #"[^A-Za-z0-9_]+" "-") #"^-|-$" "")))
 
 (defn in-coll? [elem coll] (boolean (some #{elem} coll)))
+
+(defn strip-trailing-slash [s] (if (= \/ (last s)) (apply str (butlast s)) s))
