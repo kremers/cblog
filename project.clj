399d7efcbaa38ca567daafd06b4041fad3a5cf2b
Martin Kremers <kremers@act-online.de>
2012-11-18 17:47:55 +0100

    Removed min-lein-version because it ignores ENV settings

--- project.clj
+++ project.clj
@@ -19,7 +19,6 @@
                  [org.clojars.mikejs/ring-etag-middleware "0.1.0-SNAPSHOT"]
                  ]
    :main cblog.core
-   :min-lein-version "2.0.0"
    :source-path "src"
    :jvm-opts ["-server"
               "-Djava.awt.headless=true" 
