f0311746b162ad84d0d9c31848435bcf1897b949
Martin Kremers <kremers@act-online.de>
2012-11-18 17:41:16 +0100

    Lein2 next try

--- src/cblog/core.clj
+++ src/cblog/core.clj
@@ -11,6 +11,6 @@
 (try (let [mongo-url (System/getenv "MONGOHQ_URL")] (do (connect-via-uri! mongo-url) (info (str "MONGOHQ_URL: " mongo-url)))) 
   (catch NullPointerException e (do (info "MONGOHQ_URL not set, using localhost") (connect-to-db!))))
 
-(defn -main [] (try (let [port (Integer/parseInt (System/getenv "PORT"))] (do (run-jetty routes {:port port}) (info (str "PORT: " port))))
+(defn -main [] (try (let [port (Integer. (System/getenv "PORT"))] (do (run-jetty routes {:port port}) (info (str "PORT: " port))))
   (catch Exception e (do (info "PORT not set, using 8080") (run-jetty routes {:port 8080})))))
 
